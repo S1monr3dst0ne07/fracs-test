c957e02831a4c109af4342580d527b3a8fb2e2be
S1mon <jaforsi.kozak@gmail.com>
2024-12-19 15:36:13 +0100

    implemented generic frac subtype vint fixed addition sign handling

--- fracs.h
+++ fracs.h
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
+
+
+typedef uint32_t vint;
+#define SIGN_BIT (1 << (sizeof(vint)*8 - 1)) 
 
 typedef struct 
 {
     bool sign;
-    uint64_t num;
-    uint64_t den;
+    vint num;
+    vint den;
 
 } frac;
 
-frac value2frac(uint64_t x)
+frac value2frac(vint x)
 {
     return (frac){
         .sign = false,
@@ -19,13 +24,14 @@ frac value2frac(uint64_t x)
     };
 }
 
-uint64_t frac2value(frac x)
+double frac2value(frac x)
 {
-    return (x.num / x.den) * (x.sign ? -1 : 1);
+    return ((double)x.num / x.den) * (x.sign ? -1 : 1);
 }
 
-uint64_t gcd(uint64_t a, uint64_t b)
+vint gcd(uint64_t a, uint64_t b)
 {
+    if ((a | b) == 0) return 1;
     if (a == 0) return b;
     if (b == 0) return a;
 
@@ -33,12 +39,12 @@ uint64_t gcd(uint64_t a, uint64_t b)
         if (a > b) a -= b;
         else       b -= a;
 
-    return a;
+    return a ? a : 1;
 }
 
 frac normalize(frac x)
 {
-    uint64_t f = gcd(x.num, x.den);
+    vint f = gcd(x.num, x.den);
     x.num /= f;
     x.den /= f;
 
@@ -47,10 +53,26 @@ frac normalize(frac x)
 
 frac add(frac a, frac b)
 {
+
+    vint sharedNumA = a.num * b.den;
+    vint sharedNumB = b.num * a.den;
+    vint sharedDen  = a.den * b.den;
+
+    assert((sharedNumA & SIGN_BIT) == 0);
+    assert((sharedNumA & SIGN_BIT) == 0);
+
+    if (a.sign) sharedNumA *= -1;
+    if (b.sign) sharedNumB *= -1;
+
+    vint resultNum = sharedNumA + sharedNumB;
+
+    bool sign = (resultNum & SIGN_BIT) != 0;
+    if (sign) resultNum *= -1;
+
     return normalize((frac){
-        .sign = (a.sign       ^ b.sign      ),
-        .num  = (a.num*b.den  + b.num*a.den ),
-        .den  = (a.den        * b.den       )
+        .sign = sign,
+        .num  = resultNum,
+        .den  = sharedDen
     });
 }
 
@@ -58,11 +80,6 @@ frac neg(frac x)
 {
     x.sign ^= 1;
     return x; 
-}
-
-frac sub(frac a, frac b)
-{
-    return add(a, neg(b));
 }
 
 
